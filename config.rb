f1b3a797d1c611d7f8890ae95119c9ae5b85ae97
Philippe Nénert (ALOLI sas) <philippe@aloli.fr>
2020-06-09 20:03:05 +0200

    Première version du site

--- config.rb
+++ config.rb
@@ -1,17 +1,24 @@
+require 'active_support'
+
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+activate :asciidoc
+
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+  prefix.browsers = 'last 2 versions'
 end
+
+# activate :i18n, mount_at_root: :fr
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+# page '/index.html', layout: false
+# page '/*.json',     layout: false
+# page '/*.txt',      layout: false
+# page '/*.xml',      layout: false
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
